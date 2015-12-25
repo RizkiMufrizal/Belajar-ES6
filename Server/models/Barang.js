adbd82d5e164a50a8e73b64672cd8e48ce096ade
RizkiMufrizal <mufrizalrizki@gmail.com>
2015-12-25 16:25:05 +0700

    tambah model barang

---
+++ Server/models/Barang.js
@@ -0,0 +1,31 @@
+(function() {
+  'use strict';
+
+  var barang,
+    mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+  barang = new Schema({
+    idBarang: {
+      type: 'String',
+      required: true
+    },
+    namaBarang: {
+      type: 'String',
+      required: true
+    },
+    hargaBarang: {
+      type: 'Number',
+      required: true
+    },
+    tanggalKadaluarsa: {
+      type: 'Date',
+      required: true
+    }
+  }, {
+    collection: 'tb_barang'
+  });
+
+  module.exports = mongoose.model('Barang', barang);
+
+}).call(this);
