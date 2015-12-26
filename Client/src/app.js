97e51010c755bd04054c04e1e6ece1e2f4c71926
RizkiMufrizal <mufrizalrizki@gmail.com>
2015-12-26 21:44:11 +0700

    update app config

--- Client/src/app.js
+++ Client/src/app.js
@@ -1,4 +1,5 @@
 import 'angular-material/angular-material.min.css';
+import './app.css';
 
 import angular from 'angular';
 import angularUiRouter from 'angular-ui-router';
@@ -6,7 +7,6 @@ import angularAnimate from 'angular-animate';
 import angularAria from 'angular-aria';
 import angularMessages from 'angular-messages';
 import angularMaterial from 'angular-material';
-import angularMaterialIcons from 'angular-material-icons';
 import angularLazyLoad from 'oclazyload';
 
 import appModule from './app.module';
@@ -19,7 +19,6 @@ angular.element(document).ready(function() {
       angularAria,
       angularMessages,
       angularMaterial,
-      angularMaterialIcons,
       angularUiRouter,
       angularLazyLoad,
       appModule
