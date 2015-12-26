a70fc144d9ce8515e5c6a947912a210ce07e0bf5
RizkiMufrizal <mufrizalrizki@gmail.com>
2015-12-26 11:24:00 +0700

    tambah module barang

--- Client/src/app.module.js
+++ Client/src/app.module.js
@@ -1,8 +1,12 @@
 import angular from 'angular';
 import routing from './app.config.js';
 import home from './home/home.module';
+import barang from './barang/barang.module';
 
 export default angular
-  .module('app.module', [home])
+  .module('app.module', [
+    home,
+    barang
+  ])
   .config(routing)
   .name;
