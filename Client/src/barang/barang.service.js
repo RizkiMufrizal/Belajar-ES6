5894826abcdf2d6abe583ea88597686c5f85428a
RizkiMufrizal <mufrizalrizki@gmail.com>
2015-12-26 11:25:09 +0700

    tambah barang service

---
+++ Client/src/barang/barang.service.js
@@ -0,0 +1,25 @@
+export default class BarangService {
+
+  constructor($http) {
+    this._$http = $http;
+  }
+
+  getBarang(page) {
+    return this._$http.get('/api/barang?page=' + page);
+  }
+
+  saveBarang(barang) {
+    return this._$http.post('/api/barang', barang);
+  }
+
+  updateBarang(barang, idBarang) {
+    return this._$http.put('/api/barang/' + idBarang, barang);
+  }
+
+  deleteBarang(idBarang) {
+    return this._$http.delete('/api/barang/' + idBarang);
+  }
+
+}
+
+BarangService.$inject = ['$http'];
