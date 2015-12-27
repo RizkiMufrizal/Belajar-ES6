12654d43fd3f40ec00a0f6fc8d53b321e19b217e
RizkiMufrizal <mufrizalrizki@gmail.com>
2015-12-27 22:33:29 +0700

    Update barang controller

--- Client/src/barang/barang.component/barang.controller.js
+++ Client/src/barang/barang.component/barang.controller.js
@@ -1,11 +1,16 @@
+import barangDialog from './barang.dialog.html';
+
 export default class BarangController {
 
-  constructor(BarangService) {
+  constructor(BarangService, $mdDialog) {
     this._BarangService = BarangService;
+    this._mdDialog = $mdDialog;
 
     this._paging = {
       page: 1
     };
+
+    this._inputBarang = {};
 
     this.getBarang();
 
@@ -26,14 +31,50 @@ export default class BarangController {
     }
   }
 
+  hasNextPage() {
+    if (this._paging.page < this._paging.totalPages) {
+      return false;
+    }
+    return true;
+  }
+
   previousPage() {
     if (this._paging.page > 1) {
       this._paging.page--;
       getBarang();
     }
   }
+
+  hasPreviousPage() {
+    if (this._paging.page > 1) {
+      return false;
+    }
+    return true;
+  }
   //end paging
+
+  clear() {
+    this._inputBarang.idBarang = '';
+    this._inputBarang.namaBarang = '';
+    this._inputBarang.hargaBarang = '';
+    this._inputBarang.tanggalKadaluarsa = '';
+  }
+
+  addBarang() {
+    this.clear();
+    this._mdDialog.show({
+      template: '<ng-barang-dialog></ng-barang-dialog>',
+      clickOutsideToClose: true
+    })
+      .then(function(answer) {});
+  }
+
+  saveBarang(b) {
+    this._BarangService.saveBarang(b).success((data) => {
+      alert(data.info);
+    });
+  }
 
 }
 
-BarangController.$inject = ['BarangService'];
+BarangController.$inject = ['BarangService', '$mdDialog'];
