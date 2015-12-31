41d82a953d440c9f00d08526fc95c54a540eac9b
RizkiMufrizal <mufrizalrizki@gmail.com>
2015-12-31 21:28:12 +0700

    update barang module

--- Client/src/barang/barang.module.js
+++ Client/src/barang/barang.module.js
@@ -2,12 +2,10 @@ import angular from 'angular';
 import BarangConfig from './barang.config';
 import BarangService from './barang.service';
 import BarangComponent from './barang.component/barang.component';
-import BarangDialog from './barang.component/barang.dialog';
 
 export default angular
   .module('barang', [])
   .config(BarangConfig)
   .service('BarangService', BarangService)
   .directive('ngBarang', BarangComponent)
-  .directive('ngBarangDialog', BarangDialog)
   .name;
