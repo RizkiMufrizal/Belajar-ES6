d1f56359d50947ebb1661e763076f655d29186d2
RizkiMufrizal <mufrizalrizki@gmail.com>
2015-12-31 21:30:58 +0700

    tambah baran dialog untuk save barang

--- Client/src/barang/barang.component/barang.dialog.js
+++ Client/src/barang/barang.component/barang.dialog.js
@@ -1,4 +1,28 @@
-export default function BarangDialog {
+export default function BarangDialog($mdDialog, BarangService, _inputBarang, _enable) {
 
+  let barang = this;
+
+  barang._inputBarang = _inputBarang;
+  barang.enable = _enable;
+
+  barang.cancel = () => {
+    $mdDialog.cancel();
+  };
+
+  barang.clear = () => {
+    barang._inputBarang.idBarang = '';
+    barang._inputBarang.namaBarang = '';
+    barang._inputBarang.hargaBarang = '';
+    barang._inputBarang.tanggalKadaluarsa = '';
+  };
+
+  barang.saveBarang = (b) => {
+    BarangService.saveBarang(b).success((data) => {
+      alert(data.info);
+      $mdDialog.hide();
+    });
+  };
 
 }
+
+BarangDialog.$inject = ['$mdDialog', 'BarangService', '_inputBarang', '_enable'];
