2570ee6fcb92fdaab3cbd44ba3cceae7178aad3c
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-01-01 22:42:03 +0700

    Updaye login controller

--- Client/src/login/login.component/login.controller.js
+++ Client/src/login/login.component/login.controller.js
@@ -1,13 +1,15 @@
 export default class LoginController {
 
-  constructor(LoginService, sweet) {
+  constructor(LoginService, sweet, $state) {
     this._LoginService = LoginService;
     this._sweet = sweet;
+    this._$state = $state;
   }
 
   authenticate(u) {
     this._LoginService.authenticate(u).success((data) => {
-      console.log('cek');
+      this._sweet.show('Info', 'Anda berhasil login', 'success');
+      this._$state.go('Home');
     }).error((status) => {
       this._sweet.show('Warning', 'Username dan password anda masih salah', 'error');
     });
@@ -15,4 +17,4 @@ export default class LoginController {
 
 }
 
-LoginController.$inject = ['LoginService', 'sweet'];
+LoginController.$inject = ['LoginService', 'sweet', '$state'];
