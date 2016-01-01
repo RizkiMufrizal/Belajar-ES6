8e8b7587b041814e51ac42f3fb533484804796c2
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-01-01 22:41:27 +0700

    Authenticate untuk home

--- Client/src/app.config.js
+++ Client/src/app.config.js
@@ -10,7 +10,7 @@ export default function routing($stateProvider, $locationProvider, $urlRouterPro
           template: '<ng-home></ng-home>'
         }
       },
-      authenticate: false
+      authenticate: true
     });
 
 }
--- Client/src/login/login.component/login.controller.js
+++ Client/src/login/login.component/login.controller.js
@@ -1,9 +1,18 @@
 export default class LoginController {
 
-  constructor(LoginService) {
+  constructor(LoginService, sweet) {
     this._LoginService = LoginService;
+    this._sweet = sweet;
+  }
+
+  authenticate(u) {
+    this._LoginService.authenticate(u).success((data) => {
+      console.log('cek');
+    }).error((status) => {
+      this._sweet.show('Warning', 'Username dan password anda masih salah', 'error');
+    });
   }
 
 }
 
-LoginController.$inject = ['LoginService'];
+LoginController.$inject = ['LoginService', 'sweet'];
