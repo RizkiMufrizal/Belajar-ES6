527a48f8afcec8fa46889010a260ff9a6e5026bd
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-01-01 23:04:30 +0700

    tambah module user

--- Client/src/app.module.js
+++ Client/src/app.module.js
@@ -4,12 +4,14 @@ import running from './run.config';
 import home from './home/home.module';
 import barang from './barang/barang.module';
 import login from './login/login.module';
+import user from './user/user.module';
 
 export default angular
   .module('app.module', [
     home,
     barang,
-    login
+    login,
+    user
   ])
   .config(routing)
   .run(running)
