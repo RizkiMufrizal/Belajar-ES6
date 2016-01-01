efd28bf1a7b58c58c825129d32108e82669aade6
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-01-01 18:42:19 +0700

    tambah module login

--- Client/src/app.module.js
+++ Client/src/app.module.js
@@ -1,12 +1,16 @@
 import angular from 'angular';
-import routing from './app.config.js';
+import routing from './app.config';
+import running from './run.config';
 import home from './home/home.module';
 import barang from './barang/barang.module';
+import login from './login/login.module';
 
 export default angular
   .module('app.module', [
     home,
-    barang
+    barang,
+    login
   ])
   .config(routing)
+  .run(running)
   .name;
