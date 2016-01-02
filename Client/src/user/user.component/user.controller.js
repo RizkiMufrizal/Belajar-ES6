3794549d682818fbbf64cdc33a045f584db48de4
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-01-02 17:18:12 +0700

    update untuk user controller

--- Client/src/user/user.component/user.controller.js
+++ Client/src/user/user.component/user.controller.js
@@ -1,5 +1,22 @@
-export default class userController {
-  constructor() {
-    this.nama = 'rizki';
+export default class UserController {
+
+  constructor($mdDialog, UserService, sweet) {
+    this._$mdDialog = $mdDialog;
+    this._UserService = UserService;
+    this._sweet = sweet;
   }
+
+  cancel() {
+    this._$mdDialog.cancel();
+  }
+
+  register(r) {
+    this._UserService.register(r).success((data) => {
+      this._sweet.show('Info', data.info, 'success');
+      this._$$mdDialog.hide();
+    });
+  }
+
 }
+
+UserController.$inject = ['$mdDialog', 'UserService', 'sweet'];
