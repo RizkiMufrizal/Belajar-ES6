6a18f52f249f5b4287286a110222194de6340c5d
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-01-02 19:58:04 +0700

    update home component

--- Client/src/home/home.component/home.component.js
+++ Client/src/home/home.component/home.component.js
@@ -1,9 +1,12 @@
+import homeController from './home.controller';
 import homeTemplate from './home.template.html';
 
 export default function () {
   return {
     restrict: 'E',
     scope: {},
-    template: homeTemplate
+    template: homeTemplate,
+    controller: homeController,
+    controllerAs: 'home'
   };
 }
